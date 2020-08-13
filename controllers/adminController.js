58574bacacc241ab244024e8329d8cf3c9214926
S. M. Jubayer <sadatjubayer@gmail.com>
2020-08-13 12:18:21 +0600

    M1M2F3

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,13 +11,21 @@ router.get('/', (req, res) => {
       adminCount++;
     }
   });
-
   const data = {
     totalUsers: users.length,
     admins: adminCount,
   };
-
   res.render('admin/dashboard', { data });
+});
+
+// Add Employee Route
+router.get('/AddEmployee', (req, res) => {
+  res.render('admin/AddEmployee');
+});
+
+// Get all Employee Route
+router.get('/AllEmployeeList', (req, res) => {
+  res.render('admin/AllEmployeeList', { users });
 });
 
 module.exports = router;
